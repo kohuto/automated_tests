103e35b036175290119d5e592449d4458c967b73
User Name <email@example.com>
2023-08-28 20:55:34 +0200

    updated

--- test_heap_sort.py
+++ test_heap_sort.py
@@ -3,19 +3,19 @@ from heap_sort import heapSort
 def test_heap_sort():
     arr = [12, 11, 13, 5, 6, 7]
     heapSort(arr)
-    assert arr == [5, 6, 7, 11, 12, 13]
+    assert arr == [5, 6, 7, 11, 12, 13], "Test heap_sort selhal: Neočekávaný výstup."
 
 def test_empty_list():
     arr = []
     heapSort(arr)
-    assert arr == []
+    assert arr == [], "Test empty_list selhal: Neočekáváno změny v prázdném seznamu."
 
 def test_sorted_list():
     arr = [1, 2, 3, 4, 5]
     heapSort(arr)
-    assert arr == [1, 2, 4, 5]
+    assert arr == [1, 2, 4, 5], "Test sorted_list selhal: Neočekávaný výstup pro seřazený seznam."
 
 def test_reverse_list():
     arr = [5, 4, 3, 2, 1]
     heapSort(arr)
-    assert arr == [1, 2, 3, 4, 5]
+    assert arr == [1, 2, 3, 4, 5], "Test reverse_list selhal: Neočekávaný výstup pro obrácený seznam."
