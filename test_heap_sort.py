a9798bce4580bc6527d7ed999e1ae7663d6e2c92
User Name <email@example.com>
2023-08-28 20:05:23 +0200

    updated

--- test_heap_sort.py
+++ test_heap_sort.py
@@ -1,27 +1,21 @@
-import unittest
 from heap_sort import heapSort
 
-class TestHeapSort(unittest.TestCase):
+def test_heap_sort():
+    arr = [12, 11, 13, 5, 6, 7]
+    heapSort(arr)
+    assert arr == [5, 6, 7, 11, 12, 13]
 
-    def test_heap_sort(self):
-        arr = [12, 11, 13, 5, 6, 7]
-        heapSort(arr)
-        self.assertEqual(arr, [5, 6, 7, 11, 12, 13])
+def test_empty_list():
+    arr = []
+    heapSort(arr)
+    assert arr == []
 
-    def test_empty_list(self):
-        arr = []
-        heapSort(arr)
-        self.assertEqual(arr, [])
+def test_sorted_list():
+    arr = [1, 2, 3, 4, 5]
+    heapSort(arr)
+    assert arr == [1, 2, 3, 4, 5]
 
-    def test_sorted_list(self):
-        arr = [1, 2, 3, 4, 5]
-        heapSort(arr)
-        self.assertEqual(arr, [1, 2, 3, 4, 5])
-
-    def test_reverse_list(self):
-        arr = [5, 4, 3, 2, 1]
-        heapSort(arr)
-        self.assertEqual(arr, [1, 2, 3, 4, 5])
-
-if __name__ == '__main__':
-    unittest.main()
+def test_reverse_list():
+    arr = [5, 4, 3, 2, 1]
+    heapSort(arr)
+    assert arr == [1, 2, 3, 4, 5]
