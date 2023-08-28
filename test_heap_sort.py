33fdafffe2f7fa088f029b0dfa3f609045187751
User Name <email@example.com>
2023-08-28 21:07:31 +0200

    updated

--- test_heap_sort.py
+++ test_heap_sort.py
@@ -1,21 +1,33 @@
 from heap_sort import heapSort
 
 def test_heap_sort():
-    arr = [12, 11, 13, 5, 6, 7]
-    heapSort(arr)
-    assert arr == [5, 6, 7, 11, 12, 13], "Test heap_sort selhal: Neočekávaný výstup."
+    try:
+        arr = [12, 11, 13, 5, 6, 7]
+        heapSort(arr)
+        assert arr == [5, 6, 7, 11, 12, 13]
+    except AssertionError:
+        raise AssertionError("Test heap_sort selhal: Neočekávaný výstup.")
 
 def test_empty_list():
-    arr = []
-    heapSort(arr)
-    assert arr == [], "Test empty_list selhal: Neočekáváno změny v prázdném seznamu."
+    try:
+        arr = []
+        heapSort(arr)
+        assert arr == []
+    except AssertionError:
+        raise AssertionError("Test empty_list selhal: Neočekáváno změny v prázdném seznamu.")
 
 def test_sorted_list():
-    arr = [1, 2, 3, 4, 5]
-    heapSort(arr)
-    assert arr == [1, 2, 4, 5], "Test sorted_list selhal: Neočekávaný výstup pro seřazený seznam."
+    try:
+        arr = [1, 2, 3, 4, 5]
+        heapSort(arr)
+        assert arr == [1, 2, 4, 5]
+    except AssertionError:
+        raise AssertionError("Test sorted_list selhal: Neočekávaný výstup pro seřazený seznam.")
 
 def test_reverse_list():
-    arr = [5, 4, 3, 2, 1]
-    heapSort(arr)
-    assert arr == [1, 2, 3, 4, 5], "Test reverse_list selhal: Neočekávaný výstup pro obrácený seznam."
+    try:
+        arr = [5, 4, 3, 2, 1]
+        heapSort(arr)
+        assert arr == [1, 2, 3, 4, 5]
+    except AssertionError:
+        raise AssertionError("Test reverse_list selhal: Neočekávaný výstup pro obrácený seznam.")
